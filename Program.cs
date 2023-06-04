cce147ef6fb6d608d8c628d69f859362ad2bce7a
Dioneer <Gulkina_em@mail.ru>
2023-06-04 14:01:33 +0300

    add ask function

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам
+
+using System;
+namespace ControlWork
+{
+	class ControlWork
+	{
+		static void Main()
+		{
+			Console.Clear();
+			System.Console.WriteLine("Enter number off array");
+			int length = int.Parse(Console.ReadLine()!);
+			string[] Array = CreateArr(length);
+			ShowArr(Array);
+			string[] result = CreateNewArr(Array);
+			ShowArr(result);
+		}
+
+		public static string[] CreateArr(int length)
+		{
+			string[] Arr = new string[length];
+			for (int i = 0; i < Arr.GetLength(0); i++)
+			{
+				System.Console.WriteLine($"Enter string to {i + 1} elem");
+				string input = Console.ReadLine()!;
+				Arr[i] = input;
+			}
+			return Arr;
+		}
+	}
+}
